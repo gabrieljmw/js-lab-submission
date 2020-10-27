ce55a76b1854414d57a129f0654884176d67c006
Gabriel Jahbez <45593692+gabrieljmw@users.noreply.github.com>
2020-10-26 23:20:36 -0400

    Getting set up

---
+++ README.md
@@ -0,0 +1,9 @@
+The labs submitted in this deliverable:
+
+Variables (5 Exercises)
+Function (5 Exercises)
+Document Output
+- In HTML, create an <ol>
+- Within the ordered-list, add 10 <li>
+- Give each list-item a unique id (ex1, ex2, etc, for example)
+- Instead of outputting to the console, put the string output from the previous 10    exercises to the document, with each string of output going into its own list item. (There are a few ways to write this solution, consideration given to methods that are more efficient - as long as they're still readable and logical)
---
+++ script.js
@@ -0,0 +1,16 @@
+//EXERCISE 1: The Fortune Teller
+/* Store the following into variables: job title, geographic location, annual salary, company name. 
+Output your fortune to the console like so: "You will be a X in Y, making $N for Z." */
+
+
+//EXERCISE 2: The Age Calculator
+//Store the current year in a variable. Store their birth year in a variable. Calculate their age based on the stored values. Output them to the console like so: "They are NN years old.", substituting the values.
+
+//EXERCISE 3: The Lifetime Supply Calculator
+//Store your current age into a variable. Store a maximum age into a variable. Store an estimated amount per day (as a number). Calculate how many you would eat total for the rest of your life. Output the result to the console like so: "You will need NN to last you until the ripe old age of X".
+
+//EXERCISE 4: The Geometrizer
+//Calculate properties of a circle, using the definitions here. Store a radius into a variable. Calculate the circumference based on the radius, and output "The circumference is NN". Calculate the area based on the radius, and output "The area is NN".
+
+//EXERCISE 5: The Temperature Converter
+//Store a celsius temperature into a variable. Convert it to fahrenheit and output "NN°C is NN°F".  Now store a fahrenheit temperature into a variable. Convert it to celsius and output "NN°F is NN°C."
